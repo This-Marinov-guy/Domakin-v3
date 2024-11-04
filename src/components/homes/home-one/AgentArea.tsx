cc5c9d851a5dd4821f08d176cf95eea1b62500f7
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-11-04 08:13:29 +0200

    half done agents

--- src/components/homes/home-one/AgentArea.tsx
+++ src/components/homes/home-one/AgentArea.tsx
@@ -1,97 +1,118 @@
-"use client"
-import Image from "next/image"
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
 import Link from "next/link";
-import agent_data from "@/data/home-data/AgentData";
 import Slider from "react-slick";
 
-import titleShape from "@/assets/images/shape/title_shape_05.svg"
 import { underlineWordElement } from "@/utils/reactHelpers";
 import useTranslation from "next-translate/useTranslation";
+import AgencyDetails from "@/components/inner-pages/agency/agency-details";
+import AgencyDetailsArea from "@/components/inner-pages/agency/agency-details/AgencyDetailsArea";
 
 const AgentArea = ({ style, withDetails = false }: any) => {
+  const { t } = useTranslation("translations");
 
-   const {t} = useTranslation('translations');
+  const [agentDetailsId, setAgentDetailsId] = useState<number | null>(null);
 
-   const settings = {
-      dots: true,
-      arrows: false,
-      centerPadding: '0px',
-      slidesToShow: 4,
-      slidesToScroll: 2,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      responsive: [
-         {
-            breakpoint: 1200,
-            settings: {
-               slidesToShow: 3
-            }
-         },
-         {
-            breakpoint: 768,
-            settings: {
-               slidesToShow: 2
-            }
-         },
-         {
-            breakpoint: 576,
-            settings: {
-               slidesToShow: 1
-            }
-         }
-      ]
-   }
+  const settings = {
+    dots: true,
+    arrows: false,
+    centerPadding: "0px",
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
 
-   return (
-     <div
-       className={`agent-section-one position-relative z-1 xl-mt-120 ${
-         style ? "mt-170" : "mt-150"
-       }`}
-     >
-       <div className={`container ${style ? "container-large" : ""}`}>
-         <div className="position-relative">
-           <div className="title-one mb-85 lg-mb-50 wow fadeInLeft">
-             <h3>{underlineWordElement(t("team.property_agents"), 0)}</h3>
-           </div>
+  return (
+    <div
+      className={`agent-section-one position-relative z-1 xl-mt-120 ${
+        style ? "mt-170" : "mt-150"
+      }`}
+    >
+      <div className={`container ${style ? "container-large" : ""}`}>
+        <div className="position-relative">
+          <div className="title-one mb-85 lg-mb-50 wow fadeInLeft">
+            <h3>{underlineWordElement(t("team.property_agents"), 0)}</h3>
+          </div>
 
-           <div className="wrapper position-relative z-1">
-             <Slider {...settings} className="agent-slider-one">
-               {agent_data
-                 .filter((items) => items.page === "home_1")
-                 .map((item) => (
-                   <div key={item.id} className="item">
-                     <div className="agent-card-one position-relative">
-                       <div className="img border-20">
-                         <Image
-                           src={item.thumb}
-                           alt=""
-                           className="w-100 tran5s"
-                         />
-                       </div>
-                       <div className="text-center">
-                         <h6>{item.title}</h6>
-                         <Link href="/agent_details" className="stretched-link">
-                           {item.desc}
-                         </Link>
-                       </div>
-                     </div>
-                   </div>
-                 ))}
-             </Slider>
-           </div>
+          <div className="wrapper position-relative z-1">
+            <Slider {...settings} className="agent-slider-one">
+              {Array.from({ length: 6 }, (v, i) => i).map((item, index) => (
+                <div key={index} className="item">
+                  <div className="agent-card-one position-relative">
+                    <div style={{ height: "20em" }} className="img border-20">
+                      <Image
+                        src={`/assets/img/team/${item + 1}.jpg`}
+                        height={1000}
+                        width={1000}
+                        alt=""
+                        className="w-100 tran5s"
+                      />
+                    </div>
+                    <div className="text-center">
+                      <h6>{t(`team.members.${item}.name`)}</h6>
+                      {withDetails && (
+                        <button
+                          id={`${item}`}
+                          onClick={(e: any) => {
+                            const id = e.target.id;
+                            if (id == agentDetailsId) {
+                              setAgentDetailsId(null);
+                            } else {
+                              setAgentDetailsId(+id);
+                            }
+                          }}
+                          className={` ${
+                            style ? "btn-eight" : "btn-eight fw-normal"
+                          }`}
+                        >
+                          Details
+                        </button>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </Slider>
+          </div>
 
-           {!withDetails && <div className="section-btn text-center md-mt-60">
-             <Link
-               href="/agents"
-               className={` ${style ? "btn-eight" : "btn-one fw-normal"}`}
-             >
-               Meet Entire Team
-             </Link>
-           </div>}
-         </div>
-       </div>
-     </div>
-   );
-}
+          {!withDetails && (
+            <div className="section-btn text-center md-mt-60">
+              <Link
+                href="/agents"
+                className={` ${style ? "btn-eight" : "btn-one fw-normal"}`}
+              >
+                Meet Entire Team
+              </Link>
+            </div>
+          )}
+        </div>
+
+        <AgencyDetailsArea style={false} id={agentDetailsId}/>
+      </div>
+    </div>
+  );
+};
 
 export default AgentArea;
