bbd90e6dd43e1850fce9e06febeb38e11c8e3470
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-12-24 15:07:27 +0200

    up

--- src/components/ui/modals/CookiesModal.tsx
+++ src/components/ui/modals/CookiesModal.tsx
@@ -1,23 +1,28 @@
 "use client";
 
+import { observer } from "mobx-react-lite";
 import { useStore } from "@/stores/storeContext";
 import { COOKIE_MODAL } from "@/utils/defines";
 import { LOCAL_STORAGE_COOKIE_AGREED } from "@/utils/localstorage";
 import React, { useEffect } from "react";
 import Modal from "react-bootstrap/Modal";
+import useTranslation from "next-translate/useTranslation";
 
 const CookiesModal = () => {
   const { modalStore } = useStore();
-  
-  const showCookieModal =
-    typeof localStorage !== "undefined" &&
-    localStorage.getItem(LOCAL_STORAGE_COOKIE_AGREED);
-  
+
+  const { t } = useTranslation("translations");
+
+  const handleClose = () => {
+    modalStore.closeModal();
+    localStorage.setItem(LOCAL_STORAGE_COOKIE_AGREED, "1");
+  };
+
   useEffect(() => {
-    if (showCookieModal !== "1") {
+    if (localStorage.getItem(LOCAL_STORAGE_COOKIE_AGREED) !== "1") {
       modalStore.setActiveModal(COOKIE_MODAL);
     }
-  }, [showCookieModal]);
+  }, []);
 
   return (
     <Modal
@@ -27,20 +32,34 @@ const CookiesModal = () => {
       centered
     >
       <Modal.Header>
-        <Modal.Title id="contained-modal-title-vcenter">
-          <i className="fa-solid fa-cookie-bite"></i> Cookie Policy
+        <Modal.Title
+          className="d-flex justify-content-center align-items-center w-100"
+          id="contained-modal-title-vcenter"
+        >
+          <i
+            className="fa-solid fa-cookie-bite mr-10"
+            style={{ color: "#C88F57" }}
+          ></i>{" "}
+          {t("cookies.title")}
+          <i
+            className="fa-solid fa-cookie-bite ml-10"
+            style={{ color: "#C88F57" }}
+          ></i>{" "}
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <div className="container">
-          <p>
-            To gurantee a smooth experience we from our clients to accept the
-            mandatory cookies before using the website
-          </p>
+          <p>{t("cookies.content")}</p>
+          <button
+            onClick={handleClose}
+            className="mt-20 m-a btn-nine text-uppercase rounded-3 fw-normal"
+          >
+            {t("common.agree")}
+          </button>
         </div>
       </Modal.Body>
     </Modal>
   );
 };
 
-export default CookiesModal;
+export default observer(CookiesModal);
