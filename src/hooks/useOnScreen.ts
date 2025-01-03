eb87b9be3e99aef5c212b26c496b46d96e63c293
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2025-01-03 21:45:25 +0200

    fix

--- src/hooks/useOnScreen.ts
+++ src/hooks/useOnScreen.ts
@@ -3,15 +3,21 @@ import { useEffect, useMemo, useState } from "react";
 export default function useOnScreen(ref: any) {
   const [isIntersecting, setIntersecting] = useState(false);
 
-  // const observer = () =>
-  //   new IntersectionObserver(([entry]) =>
-  //     setIntersecting(entry.isIntersecting)
-  //   );
+  const observer = useMemo(() => {
+    if (typeof window !== "undefined") {
+      return new IntersectionObserver(([entry]) =>
+        setIntersecting(entry.isIntersecting)
+      );
+    }
+    return null;
+  }, [ref]);
 
-  // useEffect(() => {
-  //   observer.observe(ref.current);
-  //   return () => observer.disconnect();
-  // }, []);
+  useEffect(() => {
+    if (!observer || !ref.current) return;
+
+    observer.observe(ref.current);
+    return () => observer.disconnect();
+  }, [observer]);
 
   return ref?.current && isIntersecting;
 }
