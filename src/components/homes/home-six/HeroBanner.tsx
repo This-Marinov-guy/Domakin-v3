1952f116987c20986effa63fcf9ce9a1aaf44d25
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2025-01-11 21:42:50 +0200

    add bgs and fix landing

--- src/components/homes/home-six/HeroBanner.tsx
+++ src/components/homes/home-six/HeroBanner.tsx
@@ -9,38 +9,38 @@ const HeroBanner = () => {
   const { t, lang } = useTranslation("translations");
 
   return (
-    <div className="hero-banner-six z-2 pt-140 lg-pt-150">
-      <div className="container container-large">
+    <div className="hero-banner-six z-2 pt-120">
+      <div className="container">
         <div className="position-relative">
           <div className="row landing">
             <Image
-              className="col-lg-6 col-12 position-relative pe-xxl-5 wow fadeInLeft"
+              className="col-lg-3 col-md-3 col-12 position-relative wow fadeInLeft"
               src={`/assets/img/landing/1-${lang}.png`}
               height={1000}
               width={1000}
               alt="Landing One"
             />
-            <div className="col-lg-6 col-12">
-              <div className="ps-xxl-5 pt-35 wow fadeInRight">
+            <div className="col-xl-6 col-lg-4 col-md-12 col-12">
+              <div className="pt-35 wow fadeInRight">
                 <h5
-                  className="color-dark hero-heading mb-40 lg-mb-20"
+                  className="color-dark hero-heading mb-40"
                   style={{ color: "#004aad" }}
                 >
                   {t("home.company_for_accommodation_searching")}
                 </h5>
-                <p className="color-dark sub-heading mb-40 lg-mb-20">
+                {/* <p className="color-dark sub-heading mb-40 lg-mb-20">
                   <i
                     className="fa-solid fa-house"
                     style={{ color: "#004aad" }}
                   ></i>{" "}
                   {t("home.find_your_place_to_live")}
-                </p>
+                </p> */}
               </div>
             </div>
           </div>
           <div className="row">
             <div className="col-12">
-              <div style={{height: '30em'}} />
+              <div style={{ height: "30em" }} />
             </div>
           </div>
           {/* <div className="row">
