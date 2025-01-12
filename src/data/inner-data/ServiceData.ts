578d1205e349c0e13e3b1ad7b715f59b993478c5
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2025-01-12 10:03:48 +0200

    update service images

--- src/data/inner-data/ServiceData.ts
+++ src/data/inner-data/ServiceData.ts
@@ -22,7 +22,7 @@ const service_data: DataType[] = [
   {
     id: 1,
     page: "service_1",
-    icon: "/assets/img/icons/icon-img/22.png",
+    icon: "/assets/img/icons/10.png",
     title: "features.viewings",
     desc: "features.agents_service",
     link: "/services/viewing",
@@ -30,7 +30,7 @@ const service_data: DataType[] = [
   {
     id: 2,
     page: "service_1",
-    icon: "/assets/img/icons/icon-img/23.png",
+    icon: "/assets/img/icons/11.png",
     title: "features.rent_an_apartment",
     desc: "features.property_inquiry",
     link: "/services/renting",
@@ -38,7 +38,7 @@ const service_data: DataType[] = [
   {
     id: 3,
     page: "service_1",
-    icon: "/assets/img/icons/icon-img/21.png",
+    icon: "/assets/img/icons/12.png",
     title: "features.list_a_room",
     desc: "features.search_for_new_flatmate_or_transfer_contract",
     link: "/services/add-listing",
