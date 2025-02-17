3d4ea250d89d81a5e76517c69058a07fa33e4403
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2025-02-17 07:58:04 +0200

    up callbacks structure

--- src/modals/AuthModal.tsx
+++ src/modals/AuthModal.tsx
@@ -95,7 +95,7 @@ const AuthModal = () => {
         provider,
         options: {
           redirectTo: new URL(
-            "/auth/callback",
+            "/callbacks/auth",
             process.env.NEXT_PUBLIC_URL
           ).toString(),
         },
