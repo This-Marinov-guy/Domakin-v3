fab9064c6c26c8cdfd8568b04a506313417985ec
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2025-07-25 22:59:55 +0300

    add GTM

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,32 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google tag (gtag.js) */}
+        {process.env.NEXT_PUBLIC_GTM_ENABLE == '1' && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GTM_ID}');
+              `}
+            </Script>
+          </>
+        )}
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
-
