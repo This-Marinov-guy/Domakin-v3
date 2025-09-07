b50aa6f077948296cd76e7f3bab4215ac82e17e3
LazarPopov <49714881+LazarPopov@users.noreply.github.com>
2025-09-07 19:27:56 +0200

    Update _document.tsx adding HTML Tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Ahrefs site verification */}
+        <meta
+          name="ahrefs-site-verification"
+          content="5bb9c9b954a6dcbe124f256875661b8543d910643c63a6209a7064ec74b56183"
+        />
+
         {/* Google tag (gtag.js) */}
         {process.env.NEXT_PUBLIC_GTM_ENABLE === "1" && (
           <>
