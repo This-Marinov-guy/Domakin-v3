75b63c1143414b1a20f1a4dd00a3dca336254e5a
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2025-12-14 13:27:45 +0200

    enhance seo

--- src/pages/seo.tsx
+++ src/pages/seo.tsx
@@ -37,15 +37,15 @@ const SEO = () => {
     description: [
       {
         "@language": "en",
-        "@value": "Platform for helping with apartment searching in Groningen, Netherlands. We offer viewings, landlord connections, and room takeovers."
+        "@value": "Platform for helping with apartment searching in the Netherlands. We offer viewings, landlord connections, and room takeovers."
       },
       {
         "@language": "bg",
-        "@value": "Платформа за подпомагане при търсенето на квартири в Гронинген, Нидерландия. Предлагаме огледи, връзка с наемодател и поемане на стаи."
+        "@value": "Платформа за подпомагане при търсенето на квартири в Нидерландия. Предлагаме огледи, връзка с наемодател и поемане на стаи."
       },
       {
         "@language": "el", // ISO code for Greek
-        "@value": "Πλατφόρμα για βοήθεια στην αναζήτηση διαμερισμάτων στο Γκρόνινγκεν, Ολλανδία. Προσφέρουμε προβολές, επαφή με ιδιοκτήτες και ανάληψη δωματίων."
+        "@value": "Πλατφόρμα για βοήθεια στην αναζήτηση διαμερισμάτων στο Ολλανδία. Προσφέρουμε προβολές, επαφή με ιδιοκτήτες και ανάληψη δωματίων."
       }
     ],
     image: "https://www.domakin.nl/assets/img/logo-2.png",
@@ -59,45 +59,54 @@ const SEO = () => {
         <>
           <meta
             name="keywords"
-            content="Real estate, Accommodations, Properties, Netherlands, Viewings, Students, Groningen, Housing"
+            content="Real estate, Accommodations, Properties, Netherlands, Viewings, Students, Housing, Rooms, Rent, Searching, Real estate careers, Viewing agent, Property agent, Amsterdam, Rotterdam, The Hague, Utrecht, Eindhoven, Groningen, Tilburg, Almere, Breda, Nijmegen, Enschede, Haarlem, Arnhem, Zaanstad, Amersfoort, Apeldoorn, Hoofddorp, Maastricht, Leiden, Dordrecht, Zoetermeer, Zwolle, Deventer, Delft, Heerlen, Alkmaar, Venlo, Leeuwarden, Hilversum, Amstelveen, Purmerend, Schiedam, Spijkenisse, Vlaardingen, Alphen aan den Rijn, Hoorn, Gouda, Zaandam, Hengelo, Velsen, Ede, Barneveld, Capelle aan den IJssel, Helmond, Roosendaal, Schagen, Vught, Katwijk, Heerenveen, Nieuwegein"
           />
           <meta
             name="description"
-            content="Platform for helping with apartment searching the town of Groningen (the Netherlands) and the area surrounding it. We offer doing the viewings for you, directly contacting you with a landlord or finding someone to take your room/apartment. Our young agents offer fast and efficient work to all our customers!"
+            content="Platform for helping with apartment searching in the Netherlands. We offer doing the viewings for you, directly contacting you with a landlord or finding someone to take your room/apartment. Our young agents offer fast and efficient work to all our customers!"
           />
         </>
       )}
-      
+
       {currentLang === "bg" && (
         <>
           <meta
             name="keywords"
-            content="Недвижими имоти, Квартири, Имоти, Нидерландия, Огледи, Студенти, Гронинген, Настаняване"
+            content="Недвижими имоти, Квартири, Имоти, Нидерландия, Огледи, Студенти, Настаняване, Стаи, Наем, Търсене, Кариери в недвижими имоти, Агент за огледи, Агент за имоти, Амстердам, Ротердам, Хага, Утрехт, Айндховен, Гронинген, Тилбург, Алмере, Бреда, Неймеген, Енсхеде, Хаарлем, Арнем, Заанстад, Амерсфорт, Апелдорн, Хофдорп, Маастрихт, Лайден, Дордрехт, Зутермер, Зволе, Девентер, Делфт, Херлен, Алкмар, Венло, Леуварден, Хилверсум, Амстелвеен, Пурмеренд, Схидам, Спайкенисе, Влардинген, Алфен ан ден Рейн, Хорн, Гауда, Заандам, Хенгело, Велсен, Еде, Барневелд, Капеле ан ден Ейсел, Хелмонд, Росендал, Схаген, Вугт, Катвейк, Херенвеен, Нивегейн"
           />
           <meta
             name="description"
-            content="Платформа за подпомагане при търсенето на квартири в Гронинген (Нидерландия) и околността. Ние можем да ви предложим да поемем огледите вместо вас, да ви свържем с наемател или да намерим човек, който да вземе вашата квартира. Нашият млад екип гарантира бърза и коректна работа с всеки клиент!"
+            content="Платформа за подпомагане при търсенето на квартири в Нидерландия. Ние можем да ви предложим да поемем огледите вместо вас, да ви свържем с наемател или да намерим човек, който да вземе вашата квартира. Нашият млад екип гарантира бърза и коректна работа с всеки клиент!"
           />
         </>
       )}
-      
+
       {currentLang === "gr" && (
         <>
           <meta
             name="keywords"
-            content="Ακίνητα, Διαμονή, Ακίνητη περιουσία, Ολλανδία, Προβολές, Φοιτητές, Γκρόνινγκεν, Στέγαση"
+            content="Ακίνητα, Διαμονή, Ακίνητη περιουσία, Ολλανδία, Προβολές, Φοιτητές, Στέγαση, Δωμάτια, Ενοικίαση, Αναζήτηση, Καριέρες ακινήτων, Πράκτορας προβολής, Πράκτορας ακινήτων, Αμστερνταμ, Ροτερνταμ, Χάγη, Ουτρέχτη, Αϊντχόβεν, Γκρόνινγκεν, Τιλμπουργκ, Άλμερε, Μπρέντα, Ναϊμεχεν, Ένσχεδε, Χάαρλεμ, Άρνεμ, Ζάανσταντ, Άμερσφοορτ, Άπελντορν, Χόφντορπ, Μάαστριχτ, Λάιντεν, Ντόρντρεχτ, Ζούτερμερ, Ζβόλε, Ντέβεντερ, Ντελφτ, Χέρλεν, Άλκμααρ, Βενλό, Λέουβαρντεν, Χίλβερσουμ, Άμστελβεν, Πούρμερεντ, Σχίνταμ, Σπάικενισσε, Βλάαρντινγκεν, Άλφεν αν ντεν Ρέιν, Χορν, Γκούντα, Ζάανταμ, Χένγκελο, Βέλσεν, Έντε, Μπάρνεβελντ, Κάπελε αν ντεν Έισσελ, Χέλμοντ, Ρόσεντααλ, Σχάγεν, Βουχτ, Κάτβέικ, Χέερενβεν, Νιουβέγκειν"
           />
           <meta
             name="description"
-            content="Πλατφόρμα για βοήθεια στην αναζήτηση διαμερισμάτων στην πόλη του Γκρόνινγκεν (Ολλανδία) και την ευρύτερη περιοχή. Προσφέρουμε τη δυνατότητα να κάνουμε τις προβολές για εσάς, να επικοινωνούμε απευθείας με τον ιδιοκτήτη ή να βρίσκουμε κάποιον για να αναλάβει το δωμάτιο/διαμέρισμά σας. Οι νέοι μας πράκτορες προσφέρουν γρήγορη και αποτελεσματική εξυπηρέτηση σε όλους τους πελάτες μας!"
+            content="Πλατφόρμα για βοήθεια στην αναζήτηση διαμερισμάτων στην πόλη του Ολλανδία. Προσφέρουμε τη δυνατότητα να κάνουμε τις προβολές για εσάς, να επικοινωνούμε απευθείας με τον ιδιοκτήτη ή να βρίσκουμε κάποιον για να αναλάβει το δωμάτιο/διαμέρισμά σας. Οι νέοι μας πράκτορες προσφέρουν γρήγορη και αποτελεσματική εξυπηρέτηση σε όλους τους πελάτες μας!"
           />
         </>
       )}
-      
+
       {/* Alternative language versions for SEO (hidden from most user agents) */}
-      <meta name="description:en" content="Platform for helping with apartment searching the town of Groningen (the Netherlands) and the area surrounding it. We offer doing the viewings for you, directly contacting you with a landlord or finding someone to take your room/apartment." />
-      <meta name="description:bg" content="Платформа за подпомагане при търсенето на квартири в Гронинген (Нидерландия) и околността. Ние можем да ви предложим да поемем огледите вместо вас, да ви свържем с наемател или да намерим човек, който да вземе вашата квартира." />
-      <meta name="description:gr" content="Πλατφόρμα για βοήθεια στην αναζήτηση διαμερισμάτων στην πόλη του Γκρόνινγκεν (Ολλανδία) και την ευρύτερη περιοχή. Προσφέρουμε τη δυνατότητα να κάνουμε τις προβολές για εσάς, να επικοινωνούμε απευθείας με τον ιδιοκτήτη." />
+      <meta
+        name="description:en"
+        content="Platform for helping with apartment searching the Netherlands. We offer doing the viewings for you, directly contacting you with a landlord or finding someone to take your room/apartment."
+      />
+      <meta
+        name="description:bg"
+        content="Платформа за подпомагане при търсенето на квартири в Нидерландия. Ние можем да ви предложим да поемем огледите вместо вас, да ви свържем с наемател или да намерим човек, който да вземе вашата квартира."
+      />
+      <meta
+        name="description:gr"
+        content="Πλατφόρμα για βοήθεια στην αναζήτηση διαμερισμάτων στην πόλη του Ολλανδία. Προσφέρουμε τη δυνατότητα να κάνουμε τις προβολές για εσάς, να επικοινωνούμε απευθείας με τον ιδιοκτήτη."
+      />
 
       <meta name="author" content="Domakin Team" />
       <script
@@ -113,8 +122,8 @@ const SEO = () => {
             content="Domakin - Housing from students to students"
           />
           <meta
-            property="twitter:description" 
-            content="Platform for helping with apartment searching in Groningen. We offer viewings, landlord connections, and room takeovers."
+            property="twitter:description"
+            content="Platform for helping with apartment searching in the Netherlands. We offer viewings, landlord connections, and room takeovers."
           />
           <meta
             name="og:title"
@@ -122,11 +131,11 @@ const SEO = () => {
           />
           <meta
             property="og:description"
-            content="Platform for helping with apartment searching in Groningen. We offer viewings, landlord connections, and room takeovers."
+            content="Platform for helping with apartment searching in the Netherlands. We offer viewings, landlord connections, and room takeovers."
           />
         </>
       )}
-      
+
       {currentLang === "bg" && (
         <>
           <meta
@@ -135,7 +144,7 @@ const SEO = () => {
           />
           <meta
             property="twitter:description"
-            content="Платформа за подпомагане при търсенето на квартири в Гронинген. Предлагаме огледи, връзка с наемодател и поемане на стаи."
+            content="Платформа за подпомагане при търсенето на квартири в Нидерландия. Предлагаме огледи, връзка с наемодател и поемане на стаи."
           />
           <meta
             name="og:title"
@@ -143,11 +152,11 @@ const SEO = () => {
           />
           <meta
             property="og:description"
-            content="Платформа за подпомагане при търсенето на квартири в Гронинген. Предлагаме огледи, връзка с наемодател и поемане на стаи."
+            content="Платформа за подпомагане при търсенето на квартири в Нидерландия. Предлагаме огледи, връзка с наемодател и поемане на стаи."
           />
         </>
       )}
-      
+
       {currentLang === "gr" && (
         <>
           <meta
@@ -170,10 +179,19 @@ const SEO = () => {
       )}
 
       {/* Alternative language versions (hidden from most user agents but helpful for SEO) */}
-      <meta name="twitter:title:en" content="Domakin - Housing from students to students" />
-      <meta name="twitter:title:bg" content="Domakin - Жилища от студенти за студенти" />
-      <meta name="twitter:title:gr" content="Domakin - Στέγαση από φοιτητές σε φοιτητές" />
-      
+      <meta
+        name="twitter:title:en"
+        content="Domakin - Housing from students to students"
+      />
+      <meta
+        name="twitter:title:bg"
+        content="Domakin - Жилища от студенти за студенти"
+      />
+      <meta
+        name="twitter:title:gr"
+        content="Domakin - Στέγαση από φοιτητές σε φοιτητές"
+      />
+
       {/* Common social media properties */}
       <meta name="twitter:image" content="./assets/img/logo-2.png" />
       <meta name="twitter:card" content="./assets/img/logo-2.png" />
@@ -190,7 +208,12 @@ const SEO = () => {
       <link rel="icon" href="./favicon.ico" type="image/x-icon" />
       <link rel="manifest" href="./manifest.json" />
       <link rel="robots" href="./robots.txt" />
-      <link rel="alternate" type="application/rss+xml" title="Domakin Blog RSS Feed" href="/api/v1/blog/rss.xml" />
+      <link
+        rel="alternate"
+        type="application/rss+xml"
+        title="Domakin Blog RSS Feed"
+        href="/api/v1/blog/rss.xml"
+      />
       {/* For IE  */}
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       {/* Chrome, Firefox OS and Opera */}
@@ -208,15 +231,31 @@ const SEO = () => {
       {(() => {
         // Get current path without language prefix
         const path = router.asPath;
-        const pathWithoutLang = path.replace(/^\/[a-z]{2}($|\/)/, '/');
-        const canonicalPath = pathWithoutLang === '/' ? '' : pathWithoutLang;
-        
+        const pathWithoutLang = path.replace(/^\/[a-z]{2}($|\/)/, "/");
+        const canonicalPath = pathWithoutLang === "/" ? "" : pathWithoutLang;
+
         return (
           <>
-            <link rel="alternate" href={`https://www.domakin.nl${canonicalPath}`} hrefLang="x-default" />
-            <link rel="alternate" href={`https://www.domakin.nl${canonicalPath}`} hrefLang="en" />
-            <link rel="alternate" href={`https://www.domakin.nl/bg${canonicalPath}`} hrefLang="bg" />
-            <link rel="alternate" href={`https://www.domakin.nl/gr${canonicalPath}`} hrefLang="el" />
+            <link
+              rel="alternate"
+              href={`https://www.domakin.nl${canonicalPath}`}
+              hrefLang="x-default"
+            />
+            <link
+              rel="alternate"
+              href={`https://www.domakin.nl${canonicalPath}`}
+              hrefLang="en"
+            />
+            <link
+              rel="alternate"
+              href={`https://www.domakin.nl/bg${canonicalPath}`}
+              hrefLang="bg"
+            />
+            <link
+              rel="alternate"
+              href={`https://www.domakin.nl/gr${canonicalPath}`}
+              hrefLang="el"
+            />
           </>
         );
       })()}
