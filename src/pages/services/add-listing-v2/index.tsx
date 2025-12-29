46c6d7e66549c51d62cd55f6d8465e166a129190
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2025-12-29 18:36:22 +0200

    modifications

--- src/pages/services/add-listing-v2/index.tsx
+++ src/pages/services/add-listing-v2/index.tsx
@@ -1,17 +1,11 @@
 import LendingPageV2 from "@/components/inner-pages/services/detail-page/LendingPageV2";
 import Wrapper from "@/layouts/Wrapper";
-import { Inter } from 'next/font/google'
-
-const inter = Inter({
-    subsets: ['latin'],
-    weight: ['400', '500', '600', '700', '800', '900'],
-})
 
 export default function index() {
     return (
         <>
             <Wrapper>
-                <LendingPageV2 className={inter.className} />
+                <LendingPageV2 />
             </Wrapper>
         </>
     );
